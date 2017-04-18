dd074adf42d1b1d4a0ee4f681b8cf78ca053f733
MWilliamsW <mxw1@email.sc.edu>
2017-04-18 18:51:55 -0400

    Create rabbit.c

---
+++ rabbit.c
@@ -0,0 +1,26 @@
+#include <iostream>
+#include "Rabbit.h"
+#include "Animal.h"
+
+using namespace std;
+
+Rabbit::Rabbit(){
+	alive =false;
+	lifeLemgth = 0;
+	nextPosition = (0,0);
+	LastEaten = 0;
+}
+
+void Rabbit::RabbitRun(bool dead, bool hasEaten, int x, int y){
+	alive = dead;
+	nextPosition(x.y);
+	lifeLength++;
+
+	if(hasEaten == false){
+	LastEaten++;
+	}
+
+	if(lifeLength > 450 || LastEaten >= 5){
+	alive = false;
+	}
+}
